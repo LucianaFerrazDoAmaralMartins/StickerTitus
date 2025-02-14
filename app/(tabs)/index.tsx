81c8a5564783a29e1dde07f9c219f7634e47c1c7
LucianaFerrazDoAmaralMartins <lucianaferrazdoamaralmartins@gmail.com>
2025-02-14 11:34:02 -0300

    chore: create Circle and Icon Button components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,57 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import Button from "@/components/Button";
+import { StyleSheet, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import ImageViewer from "@/components/ImageViewer";
+
+const PlaceholderImage = require("@/assets/images/background-image.jpg");
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
+
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
   return (
-    <View style={ styles.container }>
-      <Text style={ styles.text }>Home screen</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}>
+      <View style={styles.ImageContainer}>
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
+      </View>
+
+      {showAppOptions ? (
+        <View />
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            label="Choose a photo"
+            theme="primary"
+            onPress={pickImageAsync}
+          />
+          <Button
+            label="Use this photo"
+            onPress={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
     </View>
   );
 }
@@ -17,7 +63,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: "#25292E",
   },
-  text: {
-    color: "#FFF"
-  }
+  ImageContainer: {
+    flex: 1,
+    paddingTop: 28,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
 });
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,46 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { Pressable, StyleSheet, View, Text } from "react-native";
 
 type Props = {
   label: string;
+  theme?: "primary"; // ? para sinalizar que a prop é opcional
+  onPress?: () => void; // vai receber uma função
 };
 
-export default function Button({ label }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
+  if (theme === "primary") {
+    return (
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: "#FFD33D", borderRadius: 18 },
+        ]}
+      >
+        <Pressable
+          style={[styles.button, { backgroundColor: "#FFF" }]}
+          onPress={onPress}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color={"#25292E"}
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: "#25292E" }]}>
+            {label}
+          </Text>
+        </Pressable>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Pressable onPress={() => alert("Você clicou aqui.")}>
-        <Text>{label}</Text>
+    <View style={styles.buttonContainer}>
+      <Pressable
+        style={styles.button}
+        onPress={onPress}
+      >
+        <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
   );
@@ -30,5 +62,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     flexDirection: "row",
+  },
+  buttonLabel: {
+    color: "#FFF",
+    fontSize: 16,
+  },
+  buttonIcon: {
+    paddingRight: 8,
   },
 });
